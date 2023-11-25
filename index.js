fa130074ce949497460815f3e4b1919f25b61847
Nigam Bisoyi <nigambisoyi2004@gmail.com>
2023-11-25 16:12:31 +0530

    CRUD with mongoose

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
-const DbConnect = require("./DbConnect");
-const insert = require("./insert");
-const update = require("./update");
-const deletes = require("./delete");
-insert();
-update();
-deletes();
-
-const readdata = async () => {
-  let res = await DbConnect();
-  data = await res.find({}).toArray();
+const { default: mongoose } = require("mongoose");
+const momgoose = require("mongoose");
+momgoose.connect("mongodb://0.0.0.0:27017/API");
+const nameSchema = new mongoose.Schema({
+  name: String,
+  title: String,
+});
+const saveInDB = async () => {
+  const PracticeModel = mongoose.model("practices", nameSchema);
+  let data = new PracticeModel({ name: "rahul", title: "swain" });
+  const result = await data.save();
+  console.log(result);
+};
+// saveInDB();
+const updateInDB = async () => {
+  const info = mongoose.model("practices", nameSchema);
+  let data = await info.updateOne(
+    { name: "deepak" },
+    {
+      $set: { title: "gouda" },
+    }
+  );
   console.log(data);
 };
-readdata();
+// updateInDB();
+const deleteInDB = async () => {
+  const info = mongoose.model("practices", nameSchema);
+  const data = await info.deleteOne({ name: "deepak" });
+  console.log(data);
+};
+// deleteInDB();
+
+const readInDB = async () => {
+  const info = mongoose.model("practices", nameSchema);
+  const data = await info.find({});
+  console.log(data);
+};
+readInDB();
